9157bf1612d4c9f7a86379f568e3e8de377d92ee
Björn Stierand <bjoern-github@innovention.de>
2018-03-21 14:23:05 +0100

    merge current zsh configuration

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,1 +1,5 @@
-export PROJECT_PATHS=(~/.dotfiles ~/development)
+# set language environment
+export LANG=en_US.UTF-8
+
+# set editor
+export EDITOR=vim
---
+++ zsh/path.zsh
@@ -0,0 +1,9 @@
+[[ -z "${PATH// }" ]] || EXTPATH="${PATH}"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+[[ -z "${EXTPATH// }" ]] || {
+  export PATH="${EXTPATH}:${PATH}"
+  unset EXTPATH
+}
+
+[ -d $HOME/bin ] && export PATH="$HOME/bin:$PATH"
+[ -d $HOME/.local/bin ] && export PATH="$HOME/.local/bin:$PATH"
