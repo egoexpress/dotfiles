cca7f36622049ab8cfaefdc36d2f4b8021cddc5d
Björn Stierand <bjoern-github@innovention.de>
2018-03-22 23:05:48 +0100

    added tmux plugin manager and solarized theme

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,9 @@ if (( ! $+commands[tmuxinator] ))
 then
   sudo apt-get -yqq install tmuxinator
 fi
+
+if [ ! -d ~/.tmux-plugins ]; then
+  mkdir ~/.tmux-plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux-plugins/tpm
+  ln -s ~/.tmux-plugins ~/.tmux/plugins
+fi
