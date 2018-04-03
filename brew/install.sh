862a060b0e7f4f5e650becbb90d1ae96d494c5e0
Björn Stierand <bjoern-github@innovention.de>
2018-04-03 22:53:57 +0200

    fixed brew installation for CentOS 6

--- brew/install.sh
+++ brew/install.sh
@@ -2,8 +2,19 @@
 
 if (( ! $+commands[brew] ))
 then
-  sudo apt-get install -yqq linuxbrew-wrapper
-  brew bundle
+  if [ "$OS" = "redhat" ]
+  then
+     # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+     # see https://github.com/Linuxbrew/brew/wiki/CentOS6
+     git clone --depth 1 https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+     HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_BUILD_FROM_SOURCE=1 brew install gcc --without-glibc
+     ln -s ~/.linuxbrew/Cellar/gcc/5.5.0_3/lib/gcc/x86_64-unknown-linux-gnu/5.5.0 ~/.linuxbrew/Cellar/gcc/5.5.0_3/lib/gcc/x86_64-unknown-linux-gnu/5.5.0_3
+     brew install glibc
+  elif [ "$OS" = "debian" ]
+  then
+    install_tool linuxbrew-wrapper
+    brew bundle
+  fi
 fi
 
 if [ "$1" != "bootstrap" ]
