4d73b92a48ec6758d3d6b28f0026fde7bd4187f5
Björn Stierand <bjoern-github@innovention.de>
2018-04-03 23:39:25 +0200

    source autojump when installed with brew

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,2 +1,3 @@
 # enable autojump (https://github.com/wting/autojump)
 [ -r /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh
+[ -r ~/.linuxbrew/share/autojump/autojump.zsh ] && . ~/.linuxbrew/share/autojump/autojump.zsh
