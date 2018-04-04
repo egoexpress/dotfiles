b5d2e7293c0e1a377132b49751310e0984a5720d
Björn Stierand <bjoern-github@innovention.de>
2018-04-04 14:22:56 +0200

    install OpenSSH on Red Hat via brew (CentOS 6)
    required b/c stock openssh on CentOS 6 can't handle 'Include'
    in the .ssh/config file

--- ssh/install.sh
+++ ssh/install.sh
@@ -4,3 +4,10 @@ if (( ! $+commands[keychain] ))
 then
   install_tool keychain
 fi
+
+if [ "$OS" = "redhat" ]
+then
+  # TODO: submit patch to homebrew-core to allow
+  # OpenSSH to be built without Kerberos5
+  brew install openssh
+fi
