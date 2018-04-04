88b06339252ad78f276c9b264081949a4ad5287c
Björn Stierand <bjoern-github@innovention.de>
2018-04-04 22:10:46 +0200

    refactored OS detection

---
+++ dotfiles/env.zsh
@@ -0,0 +1,18 @@
+if [ -r /etc/redhat-release ]
+then
+  OS=redhat
+  function install_tool() {
+    brew install $1
+  }
+elif [ -r /etc/debian_version ]
+then
+  OS=debian
+  function install_tool() {
+    sudo apt-get -yqq install $1
+  }
+else
+  OS=unknown
+  function install_tool() {
+    echo "No method defined to install $1"
+  }
+fi
