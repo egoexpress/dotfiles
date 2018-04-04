bca70cfc3d95a69b0c037658ea7a99ed67b7bf44
Björn Stierand <bjoern-github@innovention.de>
2018-04-04 13:58:29 +0200

    handle grc from linuxbrew install

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,6 +1,7 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] ))
 then
-  source /etc/grc.zsh
+  [ -r /etc/grc.zsh ] && source /etc/grc.zsh
+  [ -r ~/.linuxbrew/etc/grc.zsh ] && source ~/.linuxbrew/etc/grc.zsh
   alias ls="grc --colour=auto ls --color"
 fi
