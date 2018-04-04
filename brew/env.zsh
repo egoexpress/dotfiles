89ead2261d60806240790d60707ee17a7e137439
Björn Stierand <bjoern-github@innovention.de>
2018-04-04 14:35:47 +0200

    fix variable scope of homebrew env variables

--- brew/env.zsh
+++ brew/env.zsh
@@ -2,6 +2,7 @@
   export PATH="$HOME/.local/brew/bin:$PATH"
   export MANPATH=":$HOME/.local/brew/share/man:$MANPATH"
   export INFOPATH="$HOME/.local/brew/share/info:$INFOPATH"
-  export HOMEBREW_NO_ANALYTICS=1
-  export HOMEBREW_NO_ENV_FILTERING=1
 }
+
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_ENV_FILTERING=1
