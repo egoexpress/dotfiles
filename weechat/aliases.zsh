210a99d0bbca525280f18521c575332fd8eb9b6d
Björn Stierand <bjoern-github@innovention.de>
2018-04-05 16:19:34 +0200

    added alias to backup weechat config

--- weechat/aliases.zsh
+++ weechat/aliases.zsh
@@ -2,3 +2,17 @@
 glowing-bear() {
   python -c 'import BaseHTTPServer as bhs, SimpleHTTPServer as shs; bhs.HTTPServer(("127.0.0.1", 8000), shs.SimpleHTTPRequestHandler).serve_forever()'
 }
+
+weechat-backup-config() {
+  for FILE in $(find ~/.private/weechat/weechat.relative -name "*.conf" -type f);
+  do
+    FILENAME=$(basename $FILE)
+    cp /opt/weechat/.weechat/$FILENAME $FILE
+  done
+
+  for FILE in $(find ~/.dotfiles/weechat/weechat.symlink -name "*.conf" -type f);
+  do
+    FILENAME=$(basename $FILE)
+    cp /opt/weechat/.weechat/$FILENAME $FILE
+  done
+}
