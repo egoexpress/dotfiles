f77098df11637942c092786bab75b3655dbd2b06
Björn Stierand <bjoern-github@innovention.de>
2018-06-06 16:38:44 +0200

    fix dir colors on FreeBSD

--- system/grc.zsh
+++ system/grc.zsh
@@ -3,5 +3,10 @@ if (( $+commands[grc] ))
 then
   [ -r /etc/grc.zsh ] && source /etc/grc.zsh
   [ -r ~/.linuxbrew/etc/grc.zsh ] && source ~/.linuxbrew/etc/grc.zsh
-  alias ls="grc --colour=auto ls --color"
+  if [ "${OS}" != "freebsd" ]; then
+    alias ls="grc --colour=auto ls --color"
+  else
+    export CLICOLOR=1
+    alias ls="ls -G"
+  fi
 fi
