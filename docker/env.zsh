be0ff36ccc073471824c7ecbf7c4aadc4109b09c
Björn Stierand <bjoern-github@innovention.de>
2019-01-09 07:51:59 +0100

    enable tab-completion for 'docker exec' with options

---
+++ docker/env.zsh
@@ -0,0 +1,6 @@
+
+# enable tab-completion for 'docker exec' with options
+# see https://github.com/moby/moby/commit/402caa94d23ea3ad47f814fc1414a93c5c8e7e58
+
+zstyle ':completion:*:*:docker:*' option-stacking yes
+zstyle ':completion:*:*:docker-*:*' option-stacking yes
