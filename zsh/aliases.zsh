1b0fa3caaa1cda97e27cc0b2756cd3daca012eed
Björn Stierand <bjoern-github@innovention.de>
2019-02-06 13:47:33 +0100

    make environment reload more easy (just type '.')

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,12 @@
 alias reload!='. ~/.zshrc'
 alias ll='ls -lAhL'
+
+# https://unix.stackexchange.com/questions/326911/make-and-source-default-to-zshrc/326948#326948
+_accept-line() {
+  if [[ $BUFFER == "." ]]; then
+    BUFFER="source ~/.zshrc"
+  fi
+  zle .accept-line
+}
+
+zle -N accept-line _accept-line
