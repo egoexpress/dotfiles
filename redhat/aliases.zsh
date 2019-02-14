f0dbe564721496822fa308dda118b26e8353ef92
Björn Stierand <bjoern-github@innovention.de>
2019-02-14 15:26:22 +0100

    allow ~/.local/bin in PATH

--- redhat/aliases.zsh
+++ redhat/aliases.zsh
@@ -8,9 +8,4 @@
     done
     unset TOASTPROGS
   }
-
-  # prefer toast (http://www.toastball.net/toast) path if available
-  [ -d $HOME/.toast/armed/bin ] && export PATH=$HOME/.toast/armed/bin:$PATH
-
-  [ -d $HOME/.local/bin ] && export PATH=$HOME/.local/bin:$PATH
 }
---
+++ redhat/path.zsh
@@ -0,0 +1,2 @@
+# prefer toast (http://www.toastball.net/toast) path if available
+[ -d $HOME/.toast/armed/bin ] && export PATH=$HOME/.toast/armed/bin:$PATH
