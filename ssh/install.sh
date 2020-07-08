d229ef4a56923a7b2b5b0548979df912ba8cff11
Björn Stierand <bjoern-github@innovention.de>
2020-07-08 10:16:02 +0200

    don't install openssh on Red Hat

--- ssh/install.sh
+++ ssh/install.sh
@@ -4,14 +4,3 @@ if (( ! $+commands[keychain] ))
 then
   install_tool keychain
 fi
-
-if [ "$OS" = "redhat" ]
-then
-  # TODO: submit patch to homebrew-core to allow
-  # OpenSSH to be built without Kerberos5
-  IS_INSTALLED=$(brew list | grep openssh)
-  if [ $? -ne 0 ];
-  then
-    brew install openssh
-  fi
-fi
