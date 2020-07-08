3f14af841c37c610a62ec3e788104e5078857b60
Björn Stierand <bjoern-github@innovention.de>
2020-07-08 10:57:46 +0200

    fix variable check

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,8 +1,10 @@
 # load fzf completions and key bindings
+
 [ -d /usr/share/doc/fzf ] && FZF_DIR="/usr/share/doc/fzf/examples"
 [ -d $HOME/.linuxbrew/opt/fzf/shell ] && FZF_DIR="$HOME/.linuxbrew/opt/fzf/shell"
 
-[ ${+FZF_DIR} ] && {
+[ -v FZF_DIR ] && {
   source ${FZF_DIR}/key-bindings.zsh
   source ${FZF_DIR}/completion.zsh
+  unset FZF_DIR
 }
