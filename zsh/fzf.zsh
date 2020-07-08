4268d025612a3b90c35ff54a17fee0a6c9e110e1
Björn Stierand <bjoern-github@innovention.de>
2020-07-08 09:08:17 +0200

    add zsh plugins and fzf config

---
+++ zsh/fzf.zsh
@@ -0,0 +1,5 @@
+# load fzf completions and key bindings
+[ -d /usr/share/doc/fzf ] && {
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+  source /usr/share/doc/fzf/examples/completion.zsh
+}
