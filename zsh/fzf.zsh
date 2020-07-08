4ad70c56789136ba3c23e1bafa9e7ec3ea9a5d30
Björn Stierand <bjoern-github@innovention.de>
2020-07-08 11:16:27 +0200

    add FreeBSD path

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,6 +2,7 @@
 
 [ -d /usr/share/doc/fzf ] && FZF_DIR="/usr/share/doc/fzf/examples"
 [ -d $HOME/.linuxbrew/opt/fzf/shell ] && FZF_DIR="$HOME/.linuxbrew/opt/fzf/shell"
+[ -d /usr/local/share/examples/fzf/shell ] && FZF_DIR="/usr/local/share/examples/fzf/shell"
 
 [ -v FZF_DIR ] && {
   source ${FZF_DIR}/key-bindings.zsh
