054352a1efaabbe4d0bb25c7b25de1e178536c2a
Björn Stierand <bjoern-github@innovention.de>
2020-09-10 16:48:15 +0200

    add: yet another autojump installation dir

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,6 +1,5 @@
 # enable autojump (https://github.com/wting/autojump)
 [ -r /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh
 [ -r ~/.linuxbrew/share/autojump/autojump.zsh ] && . ~/.linuxbrew/share/autojump/autojump.zsh
-
-# handle local autojump (as brew installation is broken for Linux)
 [ -r ~/.local/bin/autojump.zsh ] && . ~/.local/bin/autojump.zsh
+[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && . $HOME/.autojump/etc/profile.d/autojump.sh
