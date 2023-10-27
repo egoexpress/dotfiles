491f8876262546f7d118be281a88d1f6e92a7ee5
Björn Stierand <bjoern-github@innovention.de>
2023-10-27 11:38:00 +0200

    feat(!): change tmux plugin dir

--- tmux/install.sh
+++ tmux/install.sh
@@ -17,9 +17,9 @@ then
   fi
 fi
 
-if [ ! -d ~/.tmux-plugins ]; then
-  mkdir ~/.tmux-plugins
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux-plugins/tpm
-  git clone https://github.com/egel/tmux-gruvbox.git ~/.tmux-plugins/tmux-gruvbox
-  ln -s ~/.tmux-plugins ~/.tmux/plugins
+if [ ! -d ~/.tmux/plugins ]; then
+  mkdir ~/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/egel/tmux-gruvbox.git ~/.tmux/plugins/tmux-gruvbox
 fi
+
