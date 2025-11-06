adb14ff62f495c34a34199884a5522ccae26c3a7
Björn Stierand <bjoern-github@innovention.de>
2025-11-06 21:44:41 +0100

    fix: remove old vim files

--- vim/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias vi=vim
-alias update-vim-plugins="vim +PluginUpdate +qall"
-alias note="vim -c 'r!date' -c 'normal i# ' -c 'normal o' ~/notes.md"
--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,11 +1,3 @@
 # load fzf completions and key bindings
 
-[ -d /usr/share/doc/fzf ] && FZF_DIR="/usr/share/doc/fzf/examples"
-[ -d $HOME/.linuxbrew/opt/fzf/shell ] && FZF_DIR="$HOME/.linuxbrew/opt/fzf/shell"
-[ -d /usr/local/share/examples/fzf/shell ] && FZF_DIR="/usr/local/share/examples/fzf/shell"
-
-# [ -v FZF_DIR ] && {
-#  source ${FZF_DIR}/key-bindings.zsh
-#  source ${FZF_DIR}/completion.zsh
-#  unset FZF_DIR
-# }
+source <(fzf --zsh)
